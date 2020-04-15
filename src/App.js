39958146f8b9d2066ae69c0f2a98de9eb5302242
Jake Metz <JakeMetz1@outlook.com>
2020-04-15 21:14:01 +1000

    add source

---
+++ src/App.js
@@ -0,0 +1,33 @@
+const Pet = ({name, animal, breed}) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+}; 
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Dale",
+      animal: "Dog",
+      breed: "Retriever"
+    }),
+    React.createElement(Pet, {
+      name: "Rocket",
+      animal: "Bird",
+      breed: "Parakeet"
+    }),
+    React.createElement(Pet, {
+      name: "Fluff",
+      animal: "Cat",
+      breed: "Mixed"
+    })
+  ]);
+};
+
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById("root")
+);
